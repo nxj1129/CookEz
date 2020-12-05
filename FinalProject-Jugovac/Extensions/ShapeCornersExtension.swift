7b4405fbee409babd155c7bf5c14d0cf92f5b044
Niko Jugovac <nxj1129@g.rit.edu>
2020-12-05 17:52:25 +0100

    Design and UI changes

---
+++ FinalProject-Jugovac/Extensions/ShapeCornersExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ShapeCornersExtension.swift
+//  FinalProject-Jugovac
+//
+//  Created by Niko Jugovac on 12/5/20.
+//  Copyright © 2020 Niko Jugovac. All rights reserved.
+//
+
+import Foundation
---
+++ FinalProject-Jugovac/Views/CustomGrid.swift
@@ -0,0 +1,9 @@
+//
+//  CustomGrid.swift
+//  FinalProject-Jugovac
+//
+//  Created by Niko Jugovac on 12/5/20.
+//  Copyright © 2020 Niko Jugovac. All rights reserved.
+//
+
+import Foundation
