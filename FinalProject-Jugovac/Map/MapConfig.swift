7361ebd77bb38aa7a9420bf1e87eee00eb487fb6
Niko Jugovac <nxj1129@g.rit.edu>
2020-12-10 12:06:56 +0100

    Color changes, Map annotations

---
+++ FinalProject-Jugovac/Map/MapConfig.swift
@@ -0,0 +1,9 @@
+//
+//  MapConfig.swift
+//  FinalProject-Jugovac
+//
+//  Created by Niko Jugovac on 12/10/20.
+//  Copyright © 2020 Niko Jugovac. All rights reserved.
+//
+
+import Foundation
---
+++ FinalProject-Jugovac/Map/MapUtils.swift
@@ -0,0 +1,9 @@
+//
+//  MapUtils.swift
+//  FinalProject-Jugovac
+//
+//  Created by Niko Jugovac on 12/10/20.
+//  Copyright © 2020 Niko Jugovac. All rights reserved.
+//
+
+import Foundation
